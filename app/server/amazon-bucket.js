96fce66ff9c23dd9bc4ebbed9783078ecde487b9
James Calfee <james@jcalfee.info>
2016-10-07 17:06:49 -0500

    Remove amazon create bucket (will be done manually)

--- app/server/amazon-bucket.js
+++ app/server/amazon-bucket.js
@@ -5,5 +5,5 @@ import config from 'config'
 const {amazonBucket} = config
 
 const s3 = new AWS.S3()
-s3.createBucket({Bucket: amazonBucket})
+// s3.createBucket({Bucket: amazonBucket})
 export default s3
