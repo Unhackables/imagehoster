5b1c49e8465ea2c5e630595e779bf7e4cb8f1665
James Calfee <james@jcalfee.info>
2016-10-20 10:44:26 -0500

    Support image upload from draft.

--- app/server/upload-data.js
+++ app/server/upload-data.js
@@ -38,60 +38,67 @@ const requestDataRateLimits = [
 ]
 
 router.post('/:username/:signature', koaBody, function *() {
-    try {
-        const ip = getRemoteIp(this.req)
-        if(limit(this, requestIpRateLimits, ip, 'Uploads', 'request')) return
+    const ip = getRemoteIp(this.req)
+    if(limit(this, requestIpRateLimits, ip, 'Uploads', 'request')) return
 
-        const {files, fields} = this.request.body
+    if(missing(this, this.params, 'username')) return
+    if(missing(this, this.params, 'signature')) return
 
-        const fileNames = Object.keys(files)
-        if(missing(this, files, fileNames.length ? fileNames[0] : 'file')) return
-        if(missing(this, this.params, 'username')) return
-        if(missing(this, this.params, 'signature')) return
+    const {files, fields} = this.request.body
 
+    const fileNames = Object.keys(files)
+    const {filename, filebase64} = fields
+
+    if(!fileNames.length && !(filename && filebase64)) {
+        missing(this, {}, 'file')
+        return
+    }
+
+    const {signature} = this.params
+    const sig = parseSig(signature)
+    if(!sig) {
+        this.status = 400
+        this.statusText = `Unable to parse signature (expecting HEX data).` 
+        this.body = {error: this.statusText}
+        return
+    }
+
+    const {username} = this.params
+    const [account] = yield Apis.db_api('get_accounts', [this.params.username])
+    if(!account) {
+        this.status = 400
+        this.statusText = `Account '${this.params.username}' is not found on the blockchain.` 
+        this.body = {error: this.statusText}
+        return
+    }
+    const {posting: {key_auths}, weight_threshold, reputation} = account
+
+    const rep = repLog10(reputation)
+    if(rep < config.uploadIpLimit.minRep) {
+        this.status = 400
+        this.statusText = `Your reputation must be at least ${config.uploadIpLimit.minRep} to upload.` 
+        this.body = {error: this.statusText}
+        console.log(`Upload by '${username}' blocked: reputation ${rep} < ${config.uploadIpLimit.minRep}`);
+        return
+    }
+
+    const [[posting_pubkey, weight]] = key_auths
+    if(weight < weight_threshold) {
+        this.status = 400
+        this.statusText = `User ${username} has an unsupported posting key configuration.` 
+        this.body = {error: this.statusText}
+        return
+    }
+
+    const posting = PublicKey.fromString(posting_pubkey)
+
+    let fbuffer, fname
+    if(fileNames.length) {
         const file = files[fileNames[0]]
-        
-        const {signature} = this.params
-        const sig = parseSig(signature)
-        if(!sig) {
-            this.status = 400
-            this.statusText = `Unable to parse signature (expecting HEX data).` 
-            this.body = {error: this.statusText}
-            return
-        }
-
-        const {username} = this.params
-        const [account] = yield Apis.db_api('get_accounts', [this.params.username])
-        if(!account) {
-            this.status = 400
-            this.statusText = `Account '${this.params.username}' is not found on the blockchain.` 
-            this.body = {error: this.statusText}
-            return
-        }
-        const {posting: {key_auths}, weight_threshold, reputation} = account
-
-        const rep = repLog10(reputation)
-        if(rep < config.uploadIpLimit.minRep) {
-            this.status = 400
-            this.statusText = `Your reputation must be at least ${config.uploadIpLimit.minRep} to upload.` 
-            this.body = {error: this.statusText}
-            console.log(`Upload by '${username}' blocked: reputation ${rep} < ${config.uploadIpLimit.minRep}`);
-            return
-        }
-
-        const [[posting_pubkey, weight]] = key_auths
-        if(weight < weight_threshold) {
-            this.status = 400
-            this.statusText = `User ${username} has an unsupported posting key configuration.` 
-            this.body = {error: this.statusText}
-            return
-        }
-
-        const posting = PublicKey.fromString(posting_pubkey)
-
+        fname = file.name
         // How can I keep a multipart form upload in memory (skip the file)?
         // https://github.com/tunnckoCore/koa-better-body/issues/67
-        yield new Promise(resolve => {
+        fbuffer = yield new Promise(resolve => {
             fs.readFile(file.path, 'binary', (err, data) => {
                 if(err) {
                     console.error(err)
@@ -102,43 +109,48 @@ router.post('/:username/:signature', koaBody, function *() {
                     return
                 }
                 fs.unlink(file.path)
-
-                const megs = data.length / (1024 * 1024)
-                if(limit(this, requestDataRateLimits, username, 'Upload size', 'megabytes', megs)) {
-                    resolve()
-                    return
-                }
-
-                const dataBuffer = new Buffer(data, 'binary')
-
-                const sha = hash.sha256(dataBuffer)
-                if(!sig.verifyHash(sha, posting) && !(testKey && sig.verifyHash(sha, testKey))) {
-                    this.status = 400
-                    this.statusText = `Signature did not verify.`
-                    this.body = {error: this.statusText}
-                    resolve()
-                    return
-                }
-
-                const key = sha.toString('hex')
-                const params = {Bucket: amazonBucket, Key: key, Body: dataBuffer};
-                s3.putObject(params, (err, data) => {
-                    if(err) {
-                        console.log(err)
-                        this.status = 400
-                        this.statusText = `Error uploading ${key}.` 
-                        this.body = {error: this.statusText}
-                        resolve()
-                        return
-                    }
-                    console.log(`Uploaded s3://${amazonBucket}/${key}`);
-                    const url = `${protocol}://${host}:${port}/${key}/${file.name}`
-                    this.body = {url}
-                    resolve()
-                })
+                resolve(new Buffer(data, 'binary'))
             })
         })
-    } catch(error) {console.error(error)} 
+        if(!fbuffer)
+            return
+    } else {
+        fname = filename ? filename : ''
+        fbuffer = new Buffer(filebase64, 'base64')
+    }
+
+    const megs = fbuffer.length / (1024 * 1024)
+    if(limit(this, requestDataRateLimits, username, 'Upload size', 'megabytes', megs)) {
+        return
+    }
+
+    const sha = hash.sha256(fbuffer)
+
+    if(!sig.verifyHash(sha, posting) && !(testKey && sig.verifyHash(sha, testKey))) {
+        this.status = 400
+        this.statusText = `Signature did not verify.`
+        this.body = {error: this.statusText}
+        return
+    }
+
+    const key = sha.toString('hex')
+    const params = {Bucket: amazonBucket, Key: key, Body: fbuffer};
+    yield new Promise(resolve => {
+        s3.putObject(params, (err, data) => {
+            if(err) {
+                console.log(err)
+                this.status = 400
+                this.statusText = `Error uploading ${key}.` 
+                this.body = {error: this.statusText}
+                resolve()
+                return
+            }
+            console.log(`Uploaded '${fname}' to s3://${amazonBucket}/${key}`);
+            const url = `${protocol}://${host}:${port}/${key}/${fname}`
+            this.body = {url}
+            resolve()
+        })
+    })
 })
 
 export default router.routes()
