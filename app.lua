0e52eeeb85e8f3db4222e956ea640005f4d763f5
James Calfee <james@jcalfee.info>
2016-12-09 10:50:36 -0600

    Initial docker files. (work in progress #3)

--- app.lua
+++ app.lua
@@ -2,7 +2,7 @@ require 'quota'
 
 box.cfg {
     log_level = 5,
-    listen = '0.0.0.0:3401',
+    listen = '0.0.0.0:3301',
     slab_alloc_arena = 8.0,
     wal_dir    = "/var/lib/tarantool",
     snap_dir   = "/var/lib/tarantool",
